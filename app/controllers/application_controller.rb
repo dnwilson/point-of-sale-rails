d7d0295b9b21a6fbc87cd882776f9068850ab126
Dane Wilson <dnkw@hotmail.com>
2024-10-07 11:46:17 -0400

    Add Hotwire Native support
    Signed-off-by: Dane Wilson <dnkw@hotmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  # include Turbo::Native::Navigation
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  allow_browser versions: :modern, unless: :turbo_native_app?
+
+  # private
+
+  # def hotwire_native_app?
+  #   request.user_agent.to_s.match?(/(Turbo|Hotwire) Native/)
+  # end
 end
